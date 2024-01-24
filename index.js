7e130451ff72351f1359e2b6719f3c16ea325254
Matojeje <matojeje@gmail.com>
2024-01-24 21:03:43 +0100

    Use native checkbox instead of custom  toggle

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("hello world");
-
-function toggleClass(element) {
-    if (element.classList.contains("active-check")) {
-        element.classList.remove("active-check");
-    } else {
-        element.classList.add("active-check");
-    }
-};
